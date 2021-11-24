763b236ac5ac0a76c5817f9ff53389a6d31e7be4
ankit <ankit.f.parmar@gmail.com>
2021-11-24 23:18:39 +0530

    org and org-roam setup

--- init.el
+++ init.el
@@ -3,6 +3,10 @@
 (require 'package)
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives
+	     '("org" . "https://orgmode.org/elpa/") t)
+(add-to-list 'package-archives
+	     '("elpa" . "https://elpa.gnu.org/packages/") t)
 
 ;; (add-to-list 'package-archives
 ;; 	     '("melpa-stable" . "https://stable.melpa.org/packages/") t)
@@ -13,6 +17,14 @@
 ;; 	    ("melpa-stable" . "https://stable.melpa.org/packages/")))
 
 (package-initialize)
+
+;; use package setup
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure 't)
+
 
 ;; If there are no archived package contents, refresh them
 (when (not package-archive-contents)
@@ -38,7 +50,6 @@
             (package-install package)))
       myPackages)
 
-;; ======= Basic Customisation =======
 
 ;; ======= Python Setup =======
 (elpy-enable)
@@ -61,13 +72,13 @@
 ;; ====== Emacs Customization =======
 (setq inhibit-startup-message t)    ;; Hide the startup message
 (load-theme 'tango-dark)            ;; tango dark theme
-(global-linum-mode t)               ;; Enable line numbers globally
+;; (global-linum-mode t)               ;; Enable line numbers globally
 
-;; setting ^h as backspace and ^? as emacs help
-(global-set-key (kbd "C-?") 'help-command)
-(global-set-key (kbd "M-?") 'mark-paragraph)
-(global-set-key (kbd "C-h") 'delete-backward-char)
-(global-set-key (kbd "M-h") 'backward-kill-word)
+;; ;; setting ^h as backspace and ^? as emacs help
+;; (global-set-key (kbd "C-?") 'help-command)
+;; (global-set-key (kbd "M-?") 'mark-paragraph)
+;; (global-set-key (kbd "C-h") 'delete-backward-char)
+;; (global-set-key (kbd "M-h") 'backward-kill-word)
 
 ;; Simpleclip for simplified access to system clipboard
 (require 'simpleclip)
@@ -90,40 +101,6 @@
 (setq tramp-verbose 1)
 
 
-
-;; ;;(setq jedi:server-command ("/usr/bin/python" "/home/ankit/.emacs.d/elpa/jedi-core-20191011.1750/jediepcserver.py"))
-
-
-;; ;; enable virtual env   
-;; ;;(require 'pyvenv)
-;; ;; (pyvenv-activate "~/.anaconda3/envs/tf_gpu")
-
-;; ;; enable yasnippet, autocomplete and document generation tool.
-;; ;; (add-to-list 'load-path
-;; ;; 			 "~/.emac.d/elpa/yasnippet")
-;; ;; (require 'yasnippet)
-;; ;; (yas-global-mode 1)
-;; ;; ;;(define-key yas-minor-mode-map (kbd "SPC") yas-maybe-expand)
-;; ;; (define-key yas-minor-mode-map (kbd "C-c y") #'yas-expand)
-
-;; ;; enable line numbers
-;; ;;(setq global-linum-mode t)
-;; (global-display-line-numbers-mode 1)
-
-
-;; ;; setting default tab width
-;; (setq-default tab-width 4)
-
-
-
-;; ;; window movement using windmove keybindings
-;; ;;(windmove-default-keybindings)
-;; ;;(global-set-key (kbd "C-M-j") 'windmove-left)
-;; ;;(global-set-key (kbd "C-M-;") 'windmove-right)
-;; ;;(global-set-key (kbd "C-M-k") 'windmove-up)
-;; ;;(global-set-key (kbd "C-M-l") 'windmove-down)
-
-
 ;; always open a shell in new window
 (defun shell-other-window ()
   "Open a `shell' in a new window."
@@ -141,9 +118,9 @@
 
 (global-set-key (kbd "C-x t") #'terminal-window)
 
+
 ;; start emacs server
 ;; (server-start)
-
 
 ;; ;; edit chrome with emacs
 ;; ;; (add-to-list 'load-path "~/.emacs.d")
@@ -177,13 +154,15 @@
 
 ;; ;; (setq
 ;; ;;  message-send-mail-function     'smtpmail-send-it
- 
-;; ;; disable menu-bar too;bar and scroll bar
-;; ;; (menu-bar-mode -1)
-;; ;; (tool-bar-mode -1)
-;; ;; (scroll-bar-mode -1)
-;; ;; ;; shrink fringes/borders to 1 pixel
-;; (fringe-mode 1)
+
+
+;; disable menu-bar too;bar and scroll bar
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+;; shrink fringes/borders to 1 pixel
+(fringe-mode 1)
 
 ;; turning on 'display-time-mode'
 (setq display-time-default-load-average nil)
@@ -192,11 +171,116 @@
 ;; enable ido-mode
 (ido-mode 1)
 
+;; (keycast-mode 1)
+;; (keycast-log-mode 1)
+
+;; ============== Org Mode ============
+(org-babel-do-load-languages 'org-babel-load-languages
+                             '((python . t)))
+
+
+                               
+(add-hook 'org-mode-hook '(lambda () (visual-line-mode 1)))
+(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+
+;; gtd setup
+(setq org-agenda-files '("~/disk/org/gtd/inbox.org"
+                         "~/disk/org/gtd/gtd.org"
+                         "~/disk/org/gtd/tickler.org"))
+(setq org-todo-keywords '((sequence "TODO(t)" "WIP(w)" "|" "DONE(d)" "CANCELLED")))
+
+
+(global-set-key (kbd "C-c c") 'org-capture)
+(setq org-capture-templates
+      '(("t" "Todo [inbox]" entry (file+headline "~/disk/org/gtd/inbox.org" "Tasks")
+         "* TODO %?\n  %i\n  %a")
+        ("T" "Tickler" entry
+         (file+headline "~/disk/org/gtd/tickler.org" "Tickler")
+         "* %i% \n %U")))
+
+(setq org-refile-targets '(("~/disk/org/gtd/inbox.org" :maxlevel . 3)
+                          ("~/disk/org/gtd/gtd.org" :level . 1)
+                          ("~/disk/org/gtd/tickler.org" :maxlevel . 2)))
+
+
+
+;; Org download
+;; (require 'org-download)
+;; (add-hook 'dired-mode-hook 'org-download-enable) ;; drag and drop to dired
+(use-package org-download
+  :after org
+  :bind
+  (:map org-mode-map
+        (("s-Y" . org-download-screenshot)
+         ("s-y". org-download-yank))))
+
+;; Mathpix.el
+;; (use-package mathpix.el
+;;   :straight (:host github :repo "jethrokuan/mathpix.el")
+;;   :custom ((mathpix-app-id "app-id")
+;;            (mathpix-app-key "app-key"))
+;;   :bind
+;;   ("C-x m" . mathpix-screenshot))
+
+;; org pdf view
+(pdf-tools-install)
+;; (add-hook 'prog-mode-hook 'linum-on)
+
+;; scrot - Simple screenshot utility for emacs
+(add-to-list 'load-path "~/.emacs.d/scrot.el/scrot.el")
+;; (load "scrot")
+(require 'scrot)
+
+;; ============== Org-Roam ============
+(setq org-roam-v2-ack t)
+
+(setq org-roam-directory "/home/ankit/disk/org-roam/")
+
+(use-package org-roam
+  :after org
+  :init (setq org-roam-v2-ack t) ;; acknowledging v2 upgrade
+  :custom
+  (org-roam-directory "/home/ankit/disk/org-roam/")
+  (org-roam-completion-everywhere t)
+  :config
+  (org-roam-setup)
+   :bind (("C-c n f" . org-roam-node-find)
+         ("C-c n g" . org-roam-graph)
+         ("C-c n r" . org-roam-node-random)
+         ("C-c n c" . org-roam-capture)
+         (:map org-mode-map
+               (("C-c n i" . org-roam-node-insert)
+                ("C-c n o" . org-id-get-create)
+                ("C-c n t" . org-roam-tag-add)
+                ("C-c n a" . org-roam-alias-add)
+                ("C-c n l" . org-roam-buffer-toggle))))
+   :bind-keymap)
+
+
+
+;; (setq org-roam-db-autosync-mode t)
+;; (global-set-key (kbd "C-c n l") 'org-roam-buffer-toggle)
+;; (global-set-key (kbd "C-c n f") 'org-roam-node-find)
+;; (global-set-key (kbd "C-c n i") 'org-roam-node-insert)
+;; (setq org-roam-completion-everywhere t)
+
+;; Org Sync to Google Calendar: org-gcal
+(setq org-gcal-client-id "444741516692-nd3t72ov6k4qvs4eqnumb1vgmht2kl8q.apps.googleusercontent.com"
+      org-gcal-client-secret "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"
+      org-gcal-fetch-file-alist '(("ankit.f.parmar@gmail.com" .  "~/disk/org/schedule.org")))
+
+
+
+;; setting ^h as backspace and ^? as emacs help
+(global-set-key (kbd "C-?") 'help-command)
+(global-set-key (kbd "M-?") 'mark-paragraph)
+(global-set-key (kbd "C-h") 'delete-backward-char)
+(global-set-key (kbd "M-h") 'backward-kill-word)
+
 
 ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;EXWM CONFIG;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 
 ;; ;;(require 'exwm)
 ;; ;; (require 'exwm-config)
@@ -209,35 +293,28 @@
 
 ;; ;; set the initial number of workspaces
 ;; ;;(setq exwm-workspace-number 4)
-;; (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
-;;  '(package-selected-packages
-;;    '(flycheck yasnippet-snippets virtualenvwrapper pyenv-mode pkg-info magit jedi golden-ratio exwm elpy better-defaults ace-window ace-jump-buffer)))
-;; (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- ;; )
-
-
 
 
 ;; User-Defined init.el ends here
-(message "Done")
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "dark orange" "#8cc4ff" "#eeeeec"])
  '(package-selected-packages
-   '(simpleclip yasnippet-snippets virtualenvwrapper pyenv-mode py-autopep8 magit jedi golden-ratio flycheck exwm elpy blacken better-defaults ace-window ace-jump-buffer)))
+   '(auto-dim-other-buffers scrot org-noter-pdftools org-pdftools org-noter org-download use-package org-roam-bibtex org-gcal keycast org-roam org-bullets simpleclip yasnippet-snippets virtualenvwrapper pyenv-mode py-autopep8 jedi golden-ratio flycheck exwm elpy blacken better-defaults ace-window ace-jump-buffer)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+
+
+(message "Init Done")
+
+
