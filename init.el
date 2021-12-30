ecf327f79a283b7af8db926b5cfba5851dab564e
ankit <ankit.f.parmar@gmail.com>
2021-12-30 17:50:30 +0530

    updated init.el

--- init.el
+++ init.el
@@ -50,8 +50,16 @@
             (package-install package)))
       myPackages)
 
+;; Solution: Dark blue folder names in output of ls command in ansi term
+(setq frame-background-mode 'dark)
+
 
 ;; ======= Python Setup =======
+;; Enable 'emacs' virtualenv
+(require 'pyvenv)
+(pyvenv-activate "~/.virtualenvs/emacs_virtualenv")
+
+;; Elpy
 (elpy-enable)
 
 ;; Enable Flycheck
@@ -63,16 +71,61 @@
 (require 'py-autopep8)
 (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
 
-;; jedi hook
-(setq jedi:setup-keys t)
-(setq jedi:complete-on-dot t)
+;; jedi
 (add-hook 'python-mode-hook 'jedi:setup)
+(setq jedi:setup-keys t)
+;; (setq jedi:complete-on-dot t)
 
-  
+;; hide-show for code folding
+(load-library "hideshow")
+(global-set-key (kbd "C--") (kbd "C-u 2 C-c @ C-l"))
+;; (global-set-key (kbd "C--") 'hs-hide-level)
+(global-set-key (kbd "C-=") 'hs-toggle-hiding)
+(add-hook 'python-mode-hook 'hs-minor-mode)
+
+;; enable conda
+(require 'conda)
+;; if you want interactive shell support, include:
+(conda-env-initialize-interactive-shells)
+;; if you want eshell support, include:
+(conda-env-initialize-eshell)
+;; if you want auto-activation (see below for details), include:
+;;(conda-env-autoactivate-mode t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "dark orange" "#8cc4ff" "#eeeeec"])
+ '(conda-anaconda-home "/home/ankit/anaconda3/")
+ '(custom-enabled-themes '(tango-dark))
+ '(custom-safe-themes
+   '("c95cbb563beb3d166ba48bcc7b9d32282f15bac1c6e225feede7de36e3cd9097" default))
+ '(helm-completion-style 'helm)
+ '(package-selected-packages
+   '(pomodoro hideshow-org jedi-core epc helm projectile org-cliplink conda ein auto-dim-other-buffers scrot org-noter-pdftools org-pdftools org-noter org-download use-package org-roam-bibtex org-gcal keycast org-roam org-bullets simpleclip yasnippet-snippets pyenv-mode py-autopep8 jedi golden-ratio flycheck exwm elpy blacken better-defaults ace-window ace-jump-buffer)))
+
+(setq conda-env-home-directory (expand-file-name "~/anaconda3/"))
+(setq conda-env-home-directory (expand-file-name "~/anaconda3/") ;; as in previous example; not required
+  conda-env-subdirectory "envs")
+;; if you want to automatically activate a conda environment on the opening of a file:
+;; (add-to-hook 'find-file-hook (lambda () (when (bound-and-true-p conda-project-env-path)
+                                          ;; (conda-env-activate-for-buffer))))
+
+;; Emacs Jupyter Notebook
+(setq ein:output-area-inlined-images t)
+;; (setq ein:slice-image t)
+
+
 ;; ====== Emacs Customization =======
 (setq inhibit-startup-message t)    ;; Hide the startup message
 (load-theme 'tango-dark)            ;; tango dark theme
-;; (global-linum-mode t)               ;; Enable line numbers globally
+;; (load-theme 'manoj-dark)            ;; tango dark theme
+(global-linum-mode t)               ;; Enable line numbers globally
 
 ;; ;; setting ^h as backspace and ^? as emacs help
 ;; (global-set-key (kbd "C-?") 'help-command)
@@ -85,13 +138,26 @@
 (simpleclip-mode 1)
 
 ;; Ace window for window switching
-(global-set-key (kbd "M-o") 'ace-window)
+;; (global-set-key (kbd "M-o") 'ace-window)
+;; Below keymap works in terminal as well
+(global-set-key (kbd "C-x o") 'ace-window)
+
+;; windmove, to check if easyness compared to ace-window
+;; (global-set-key (kbd "C-x j") 'windmove-left)
+;; (global-set-key (kbd "C-x ;") 'windmove-right)
+;; (global-set-key (kbd "C-x i") 'windmove-up)
+;; (global-set-key (kbd "C-x n") 'windmove-down)
+
 
 
 ;; ===== Tramp qblocks Setup =====
-(defun connect-qblocks2 ()
+(defun connect-qblocks3 ()
   (interactive)
-  (dired "/ssh:qblocks2:/home/qblocks/"))
+  (dired "/ssh:qblocks3:/home/qblocks/"))
+
+(defun connect-qblocks4 ()
+  (interactive)
+  (dired "/ssh:qblocks4:/home/qblocks/"))
 
 (setq remote-file-name-inhibit-cache nil)
 (setq vc-ignore-dir-regexp
@@ -174,6 +240,10 @@
 ;; (keycast-mode 1)
 ;; (keycast-log-mode 1)
 
+;; Pomodoro timer
+(require 'pomodoro)
+(pomodoro-add-to-mode-line)
+
 ;; ============== Org Mode ============
 (org-babel-do-load-languages 'org-babel-load-languages
                              '((python . t)))
@@ -184,22 +254,27 @@
 (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
 
 ;; gtd setup
+;; inbox - capture tasks, while reviewing, refile the task to corresponding file, project
+;; personal - personal projects tracking
+;; vyn - projects related to vyntelligence, humanlearning pvt ltd
+;; tickler - add task with timestamp to get reminded at the right moment, it would
+;; be easier if able to integrate google calender with this
 (setq org-agenda-files '("~/disk/org/gtd/inbox.org"
-                         "~/disk/org/gtd/gtd.org"
+                         "~/disk/org/gtd/personal.org"
+                         "~/disk/org/gtd/vyn.org"
                          "~/disk/org/gtd/tickler.org"))
 (setq org-todo-keywords '((sequence "TODO(t)" "WIP(w)" "|" "DONE(d)" "CANCELLED")))
-
 
 (global-set-key (kbd "C-c c") 'org-capture)
 (setq org-capture-templates
       '(("t" "Todo [inbox]" entry (file+headline "~/disk/org/gtd/inbox.org" "Tasks")
-         "* TODO %?\n  %i\n  %a")
+         "* TODO %?\n  %i\n")
         ("T" "Tickler" entry
          (file+headline "~/disk/org/gtd/tickler.org" "Tickler")
          "* %i% \n %U")))
 
-(setq org-refile-targets '(("~/disk/org/gtd/inbox.org" :maxlevel . 3)
-                          ("~/disk/org/gtd/gtd.org" :level . 1)
+(setq org-refile-targets '(("~/disk/org/gtd/personal.org" :maxlevel . 1)
+                           ("~/disk/org/gtd/vyn.org" :level . 1)
                           ("~/disk/org/gtd/tickler.org" :maxlevel . 2)))
 
 
@@ -242,6 +317,11 @@
   :custom
   (org-roam-directory "/home/ankit/disk/org-roam/")
   (org-roam-completion-everywhere t)
+  (org-roam-mode-section-functions
+   (list 'org-roam-backlinks-section
+         'org-roam-reflinks-section
+         'org-roam-unlinked-references-section
+         ))
   :config
   (org-roam-setup)
    :bind (("C-c n f" . org-roam-node-find)
@@ -253,7 +333,9 @@
                 ("C-c n o" . org-id-get-create)
                 ("C-c n t" . org-roam-tag-add)
                 ("C-c n a" . org-roam-alias-add)
-                ("C-c n l" . org-roam-buffer-toggle))))
+                ("C-c n l" . org-roam-buffer-toggle)
+                ("C-M-i"   . completion-at-point))))
+   
    :bind-keymap)
 
 
@@ -278,6 +360,20 @@
 (global-set-key (kbd "M-h") 'backward-kill-word)
 
 
+;; projectile mode
+(use-package projectile
+  :ensure t
+  :config
+  (define-key projectile-mode-map (kbd "C-x p") 'projectile-command-map)
+  (projectile-mode +1))
+
+;; helm completion
+(require 'helm-config)
+(global-set-key (kbd "M-x") #'helm-M-x)
+(global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
+(global-set-key (kbd "C-x C-f") #'helm-find-files)
+(helm-mode 1)
+
 ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;EXWM CONFIG;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -297,21 +393,15 @@
 
 ;; User-Defined init.el ends here
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "dark orange" "#8cc4ff" "#eeeeec"])
- '(package-selected-packages
-   '(auto-dim-other-buffers scrot org-noter-pdftools org-pdftools org-noter org-download use-package org-roam-bibtex org-gcal keycast org-roam org-bullets simpleclip yasnippet-snippets virtualenvwrapper pyenv-mode py-autopep8 jedi golden-ratio flycheck exwm elpy blacken better-defaults ace-window ace-jump-buffer)))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#2e3436" :foreground "#eeeeec" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 85 :width normal :foundry "ADBO" :family "Source Code Pro"))))
+ '(font-lock-comment-delimiter-face ((t nil)))
+ '(term-color-blue ((t (:background "blue" :foreground "green3")))))
 
 
 
