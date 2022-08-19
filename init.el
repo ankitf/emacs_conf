3be68356252a008ca76b3c8dc6a85e68e302858c
ankitf <ankit.f.parmar@gmail.com>
2022-08-19 20:36:46 +0530

    some packages enabled

--- init.el
+++ init.el
@@ -1,2 +1,36 @@
 
 (org-babel-load-file "~/.emacs.d/config.org")
+
+;; A GitHub username for API authentication
+(setq grip-github-user "grip-github-user")
+
+;; A GitHub password or auth token for API auth
+(setq grip-github-password "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")
+
+
+;; (custom-set-variables
+;;  ;; custom-set-variables was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  '(ein:output-area-inlined-images t))
+;; (custom-set-faces
+;;  ;; custom-set-faces was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  )
+
+(message "Init Done")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(warning-suppress-types '((comp) (comp))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
